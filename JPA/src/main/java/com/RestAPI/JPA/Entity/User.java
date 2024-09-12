33c4ba2a83085e3935920751b1453fcd7092d490
nagmakhan1993 <158118920+nagmakhan1993@users.noreply.github.com>
2024-09-12 16:10:15 +0530

    add getters and setters

--- JPA/src/main/java/com/RestAPI/JPA/Entity/User.java
+++ JPA/src/main/java/com/RestAPI/JPA/Entity/User.java
@@ -22,4 +22,24 @@ public class User {
     private String userName;
     @Column(name = "password", nullable = false)
     private String password;
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+     
 }
